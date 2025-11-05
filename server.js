248a6d8b1fb09ea0372922c204c2300c7d74201d
yourdiebronze7 <sidnyjernov@hotmail.com>
2025-11-05 11:36:51 +0000

    Update mongoose connection options to use useFindAndModify

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ const PORT = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
-mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true })
+mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true, useFindAndModify: false })
   .then(() => console.log('MongoDB connected'))
   .catch(err => console.log(err));
 
