1ca57d89f752a50f5b45ec2cdc329fa89c828d6e
yourdiebronze7 <sidnyjernov@hotmail.com>
2025-11-05 11:37:45 +0000

    Update MongoDB URI to include a more descriptive default URI

--- config.js
+++ config.js
@@ -1,3 +1,3 @@
 module.exports = {
-  MONGODB_URI: process.env.MONGODB_URI || 'mongodb://localhost:27017/employee_management_system?retryWrites=true'
+  MONGODB_URI: process.env.MONGODB_URI || 'mongodb://localhost:27017/employee_management_system?retryWrites=true&w=majority'
 };
